317d8da7f5077e51ca09155a492b21857b1c16f3
rezwan <shahriar.rezwan@neura-robotics.com>
2025-05-25 14:42:13 +0200

    Initial commit

---
+++ test/monitoring-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MonitoringInfrastructure from '../lib/monitoring-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/monitoring-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MonitoringInfrastructure.MonitoringInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
